2991d798e57eb0474f5bd6938aca436a9c66991b
FussyReich <=>
2023-08-08 15:40:23 +0900

    open URL

--- label.py
+++ label.py
@@ -2,9 +2,13 @@ import sys
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.font as f
+import webbrowser.open(https://sushida.net/play.html)
 
 def close():
     root.destroy()
+
+def sushi():
+    import main
 
 root = tk.Tk()
 root.title("寿司打をぶっ壊す")
@@ -13,11 +17,13 @@ root.geometry("350x150")
 #メインフレームの作成
 frame=tk.Frame(root)
 frame.pack(fill=tk.BOTH, pady=10)
+SushiButton=ttk.Button(frame, text="実行する", command=sushi)
 
 #ウィジェットの作成
 Title=tk.Label(frame, text="自動で寿司打をやってもらおう()", font=("MSゴシック", "20", "bold"))
 
 #ウィジェットの配置
 Title.pack()
+SushiButton.pack(pady=10)
 
 root.mainloop()
